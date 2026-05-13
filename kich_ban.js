472f3af4562af263afce6eac313d09179b609d4a
Vinh <ocdie2212@gmail.com>
2026-05-13 19:24:48 +0700

    cập nhật nội dung làm buổi chiều

--- kich_ban.js
+++ kich_ban.js
@@ -1,2 +1,24 @@
-const NutCut = document.getElementsByID('myBtn');
-const 
+const nut_menu =document.getElementById("menu_button")
+const nut_con_menu=document.getElementsByClassName("con_menu")
+function doimaunguoc(danh_sach) {
+    for (let i = 0; i < danh_sach.length; i++) {
+        danh_sach[i].style.color = "rgb(255, 255, 255)";
+    }
+}
+function doimau(danh_sach) {
+    for (let i = 0; i < danh_sach.length; i++) {
+        danh_sach[i].style.color = "rgb(8, 8, 8)";
+    }
+}
+
+count=1
+nut_menu.addEventListener("click",function(){count+=1;if(count%2==0)
+    {doimau(nut_con_menu)}else{doimaunguoc(nut_con_menu)}})
+
+nut_menu.addEventListener("mouseenter",function(){nut_menu.style.backgroundColor="rgb(51,51,51)"})
+nut_menu.addEventListener("mouseleave",function(){nut_menu.style.backgroundColor="rgb(208,210,211)"})
+
+
+
+
+class san_pham{Tao_SanPham(ten,gia,anh,gioi_tinh="Nam",img){this.ten=ten;this.gia=gia;this.gioitinh=gioi_tinh,this.anh=img};}
